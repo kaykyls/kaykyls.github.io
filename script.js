94bea339316b3ad8beab4a2037c9ef4865f4e91c
Kayky Lopes <20211bsifsa0025@ifba.edu.br>
2022-09-12 21:26:42 -0300

    mobile menu

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger-menu")
+const navMenu = document.querySelector(".nav-menu")
+
+console.log(navMenu)
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger-menu-ativo")
+    navMenu.classList.toggle("nav-menu-ativo")
+})
+
+document.querySelectorAll(".nav-menu-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("hamburger-menu-ativo");
+    navMenu.classList.remove("nav-menu-ativo");
+}))
+
+console
