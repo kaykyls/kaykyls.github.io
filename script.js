c9a9355b78be631596f2eeea19395a3625da4158
Kayky Lopes <20211bsifsa0025@ifba.edu.br>
2022-10-23 23:53:51 -0300

    added projects section, not responsive yet

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelector(".hamburger-menu")
 const navMenu = document.querySelector(".nav-menu")
+const themeBtn = document.querySelector(".theme-btn")
+const html = document.querySelector("html")
+const themeButtons = document.querySelectorAll(".theme-btn span")
+
+console.log(themeButtons)
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger-menu-ativo")
@@ -10,3 +15,10 @@ document.querySelectorAll(".nav-menu-link").forEach(n => n.addEventListener("cli
     hamburger.classList.remove("hamburger-menu-ativo");
     navMenu.classList.remove("nav-menu-ativo");
 }))
+
+themeBtn.addEventListener("click", () => {
+    html.classList.toggle("lightmode")
+    themeButtons.forEach((button) => {
+        button.classList.toggle("hidden")
+    })
+})
