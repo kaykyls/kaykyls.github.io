6c80dc5867b727d24c6c4abdebd3e608251dfb7c
Kayky Lopes <20211bsifsa0025@ifba.edu.br>
2022-11-12 21:46:35 -0300

    css changes

--- script.js
+++ script.js
@@ -9,8 +9,18 @@ onload = () => {
         html.classList.remove("light-mode")
         html.classList.remove("dark-mode")
         html.classList.add(localStorage.getItem("theme"))
+
+        
+        themeButtons.forEach(button => {
+            button.classList.add("hidden")
+        })
+
+        if(localStorage.getItem("button") === "moon"){
+            themeButtons[0].classList.remove("hidden")
+        } else {
+            themeButtons[1].classList.remove("hidden")
+        }
     }
-    
 }
 
 hamburger.addEventListener("click", () => {
@@ -26,14 +36,26 @@ document.querySelectorAll(".nav-menu-link").forEach(n => n.addEventListener("cli
 themeBtn.addEventListener("click", toggleTheme)
 
 function toggleTheme() {
+    if (localStorage.getItem("theme")) {
+        localStorage.removeItem("theme", html.className)
+    }
+
     html.classList.toggle("light-mode")
     html.classList.toggle("dark-mode")
+
+    localStorage.setItem("theme", html.className)
+
+    toggleButtons()
+}
+
+function toggleButtons() {
     themeButtons.forEach((button) => {
         button.classList.toggle("hidden")
     })
-
-    localStorage.removeItem("theme", "light-mode dark-mode")
-    localStorage.removeItem("theme", html.className)
-    const currentTheme = html.className
-    localStorage.setItem("theme", currentTheme)
+ 
+    if(html.className === "dark-mode"){
+        localStorage.setItem("button", "sun")
+    } else {
+        localStorage.setItem("button", "moon")
+    }
 }
