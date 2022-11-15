84f2ca104f1c9a4cff230611fa48c8ec818a64bd
Kayky Lopes <20211bsifsa0025@ifba.edu.br>
2022-11-15 16:43:06 -0300

    header appearing bug fix

--- js/script.js
+++ js/script.js
@@ -66,14 +66,14 @@ let previousScroll = 0
 onscroll = e => {
     if(window.scrollY > 174 && window.scrollY > previousScroll) {
         previousScroll = window.scrollY
-        header.style.top = "-100px"
-        backToTop.style.bottom = "-100px"
+        header.style.top = "-200px"
+        backToTop.style.bottom = "-200px"
     }
     
     else if (window.scrollY < previousScroll) {
         if(window.scrollY <= 100) {
             header.classList.remove("hasBackground")
-            backToTop.style.bottom = "-100px"
+            backToTop.style.bottom = "-200px"
         } else {
             header.classList.add("hasBackground")
             backToTop.style.bottom = "2rem"
